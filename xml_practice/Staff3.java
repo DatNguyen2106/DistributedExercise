e864c3e7f163db3270a1293574fb4f5dfb127d21
DatNguyen2106 <75649450+DatNguyen2106@users.noreply.github.com>
2021-05-28 10:46:21 +0700

    Add files via upload
    xml test 3

---
+++ xml_practice/Staff3.java
@@ -0,0 +1,50 @@
+package xml_test_1;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Staff3 {
+	public static void main(String[] args) {
+		// Instantiate the Factory
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+		try {
+			// parse XML file
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(new File("src/xml_test_1/staff3.xml"));
+			NodeList nl = doc.getElementsByTagName("staff");
+			System.out.println(nl.getLength());
+
+			for (int i = 0; i < nl.getLength(); i++) {
+				Node n = nl.item(i);
+				Element e = (Element) n;
+			
+				// node staff e
+				// print
+				System.out.println("");
+				System.out.println("Staff:" + e.getAttribute("id"));
+				System.out.println("Name: " + e.getElementsByTagName("firstname").item(0).getTextContent());
+                System.out.println("Salary: " + e.getElementsByTagName("salary").item(0).getTextContent());
+                System.out.println("Nickname: " + e.getElementsByTagName("nickname").item(0).getTextContent());
+                System.out.println("salary: " + e.getElementsByTagName("salary").item(0).getTextContent());
+                Element e1 =  (Element) e.getElementsByTagName("salary").item(0);
+                System.out.println("currency: " + e1.getAttribute("currency"));
+
+			}
+
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
